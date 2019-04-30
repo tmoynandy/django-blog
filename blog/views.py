84ff2ef7de7dc65fef48cd25bb64367413f28e03
tmoynandy <tmoy.nandy@gmail.com>
2019-04-30 12:07:47 +0530

    added basic templating & bootstrap styling

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'TanuN',
+        'title' : 'blog1',
+        'content' : 'aaaaa',
+        'date' : '9/11/1996'
+    },
+    {
+        'author' : 'Tanumoy',
+        'title' : 'blog2',
+        'content' : 'aaaaabbbb',
+        'date' : '9/11/1997'
+    },
+    {
+        'author' : 'TanuNandy',
+        'title' : 'blog3',
+        'content' : 'aaaaaccccc',
+        'date' : '9/11/1998'
+    },
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context, {'title' : 'Home'})
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
 
