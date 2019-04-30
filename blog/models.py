a5f57a6020b67fe1755d070e59277d9ccac99768
tmoynandy <tmoy.nandy@gmail.com>
2019-04-30 14:18:28 +0530

    added Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+##user is imported from the already created user model that shows in admin
+from django.contrib.auth.models import User
 
 # Create your models here.
+#each class will be a table in the db
+#each attribute in the class will be a column
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
