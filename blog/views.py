7908a67dd69ae4dcc97b7b39e29b36c0294f13f6
tmoynandy <tmoy.nandy@gmail.com>
2019-04-30 14:34:39 +0530

    fetching post from Post table

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#importing Post model
+from .models import Post
 
-posts = [
-    {
-        'author' : 'TanuN',
-        'title' : 'blog1',
-        'content' : 'aaaaa',
-        'date' : '9/11/1996'
-    },
-    {
-        'author' : 'Tanumoy',
-        'title' : 'blog2',
-        'content' : 'aaaaabbbb',
-        'date' : '9/11/1997'
-    },
-    {
-        'author' : 'TanuNandy',
-        'title' : 'blog3',
-        'content' : 'aaaaaccccc',
-        'date' : '9/11/1998'
-    },
-]
+# #dummy data
+# posts = [
+#     {
+#         'author' : 'TanuN',
+#         'title' : 'blog1',
+#         'content' : 'aaaaa',
+#         'date_posted' : '9/11/1996'
+#     },
+#     {
+#         'author' : 'Tanumoy',
+#         'title' : 'blog2',
+#         'content' : 'aaaaabbbb',
+#         'date_posted' : '9/11/1997'
+#     },
+#     {
+#         'author' : 'TanuNandy',
+#         'title' : 'blog3',
+#         'content' : 'aaaaaccccc',
+#         'date_posted' : '9/11/1998'
+#     },
+# ]
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context, {'title' : 'Home'})
 
