c4a59239402d4f82d92a18400ce527282f016bce
tmoynandy <tmoy.nandy@gmail.com>
2019-05-02 04:32:52 +0530

    added password reset | yet to add env variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,3 +134,10 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 #when someone tries to access unauthenticated url, redirect them to
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cofpythons@gmail.com'
+EMAIL_HOST_PASSWORD = '@6969SujoyDiptangsuTanumoy'
